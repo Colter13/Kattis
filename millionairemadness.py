a5938b05b939a5dcfdd66e44498ccbbd9ecea5b5
Colter Radke <ceradke1@liberty.edu>
2024-11-30 10:26:27 -0500

    Worked on millionaire madness adjacency list stack

--- millionairemadness.py
+++ millionairemadness.py
@@ -13,26 +13,49 @@ rows, cols = map(int, input().split())
 m = [list(map(int, input().split())) for i in range(rows)]
 
 length = 0
-if max(m[rows-2][cols-1], m[rows-1][cols-2]) < m[rows-1][cols-1]:
-    length = m[rows-1][cols-1] - max(m[rows-2][cols-1], m[rows-1][cols-2])
+
+# check to override the baseline length
+length1 = 0
+length2 = 0
+if rows > 1 and m[rows-2][cols-1] < m[rows-1][cols-1]:
+    length1 = m[rows-1][cols-1] - m[rows-2][cols-1]
+if cols > 1 and m[rows-1][cols-2] < m[rows-1][cols-1]:
+    length2 = m[rows-1][cols-1] - m[rows-1][cols-2]
+length = min(length1, length2)
 
 while True:
     a = {}
-    for i in range(rows):
-        for j in range(cols):
-            a[(i,j)] = []
-            if i+1 < rows and m[i][j] >= m[i+1][j] - length:
-                a[(i,j)].append((i+1,j))
-            if j+1 < cols and m[i][j] >= m[i][j+1] - length:
-                a[(i,j)].append((i,j+1))
-            if i-1 >= 0 and m[i][j] >= m[i-1][j] - length:
-                a[(i,j)].append((i-1,j))
-            if j-1 >= 0 and m[i][j] >= m[i][j-1] - length:
-                a[(i,j)].append((i,j-1))
+    stack = [(0, 0)]
+    p = [[0 for i in range(cols)] for i in range(rows)]
+    while len(stack):
+        i, j = stack.pop()
+        a[(i,j)] = []
+        if i+1 < rows and m[i][j] >= m[i+1][j] - length:
+            a[(i,j)].append((i+1,j))
+            if p[i+1][j] != 1:
+                stack.append((i+1,j))
+            p[i+1][j] = 1
+        if j+1 < cols and m[i][j] >= m[i][j+1] - length:
+            a[(i,j)].append((i,j+1))
+            if p[i][j+1] != 1:
+                stack.append((i,j+1))
+            p[i][j+1] = 1
+        if i-1 >= 0 and m[i][j] >= m[i-1][j] - length:
+            a[(i,j)].append((i-1,j))
+            if p[i-1][j] != 1:
+                stack.append((i-1,j))
+            p[i-1][j] = 1
+        if j-1 >= 0 and m[i][j] >= m[i][j-1] - length:
+            a[(i,j)].append((i,j-1))
+            if p[i][j-1] != 1:
+                stack.append((i,j-1))
+            p[i][j-1] = 1
     v = [[0 for i in range(cols)] for i in range(rows)]
-    target_neighbors = []
-    if (rows == 1 or (rows-1,cols-1) in a[(rows-2,cols-1)]) or (cols == 1 or (rows-1,cols-1) in a[(rows-1,cols-2)]):
-        if dfs(a, v, 0, 0, rows, cols):
-            print(length)
-            exit()
+    if p[rows-1][cols-1] and dfs(a, v, 0, 0, rows, cols):
+        print(length)
+        exit()
     length += 1
+
+"""
+Somehow have p saved and not reset after each length increase
+"""
